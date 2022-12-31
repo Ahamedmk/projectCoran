55e7f19547b5c6b507c23d76f80a64af3f68cd92
Ahamedmk <amkavavo1@gmail.com>
2022-12-31 12:29:15 +0100

    graphique

--- src/Components/NewCharts/Sidebar/Sidebar.js
+++ src/Components/NewCharts/Sidebar/Sidebar.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Sidebar.css'
+import imgBurger from '../../../assets/menu.png'
+import zikrLogo from '../../../assets/zikr_logo.png'
+
 import {Link, NavLink} from 'react-router-dom';
 
 export default function Sidebar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+ 
+  useEffect(() => {
+    const changeWidth = () => {
+    setLargeur(window.innerWidth);
+  }
+  window.addEventListener('resize', changeWidth);
+
+  return () => {
+    window.removeEventListener('resize',changeWidth);
+  }
+
+  }, [])
+
   return (
     <nav className='sidenav'>
-      <NavLink to="/"
+      <img className='zikr' src={zikrLogo} alt="" />
+      {(toggleMenu || largeur > 500) && (
+      <ul className='menuList'>
+      <li className='items'><NavLink to="/"
       className={({isActive}) => isActive ? "" : ""}
-      >Présentation</NavLink>
-      <NavLink to="/recap"
+      >Présentation</NavLink></li>
+      <li className='items'><NavLink to="/recap"
        className={({isActive}) => isActive ? "activeLink" : ""}
-      >Récapitulatif</NavLink>
-      <NavLink to="/dashboard"
+      >Récapitulatif</NavLink></li>
+      <li className='items'><NavLink to="/dashboard"
        className={({isActive}) => isActive ? "activeLink" : ""}
-      >Dashboard</NavLink>
+      >Dashboard</NavLink></li>
+      </ul>
+      )}
+      <button onClick={toggleNav}><img className='burger' src={imgBurger} alt="" /></button>
     </nav>
   )
 }
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,49 +1,100 @@
-// import React from 'react'
+ import React from 'react'
 // import { useState, useContext } from 'react'
 //  import './Dashboard.css'
 // import BarCharts from '../../Components/NewCharts/BarCharts'
 
 import {
   Chart as ChartJS,
-  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
   Tooltip,
   Legend
-} from 'chart.js'
+} from 'chart.js';
 
-import { Doughnut } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(
-  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
   Tooltip,
   Legend
-);
+)
+
+
+// export const options = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+      
+//     },
+//     title: {
+//       display: true,
+//       text: 'Chart.js Bar Chart',
+//     },
+//   },
+// };
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     },
+//     {
+//       label: 'Dataset 2',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     },
+//   ],
+// };
 
 export default function Dashboard() {
-  const data = {
-    labels: ['Yes', 'No'],
-    datasets: [{
-      label: 'Poll',
-      data: [3, 6],
-      backgroundColor: ['black', 'red'],
-      borderColor: ['black', 'red'],
-    }]
-  }
+    const data = {
+      labels: ['Mon', 'Tue', 'Wed', 'jeu', 'ven', 'sam', 'dim'],
+      datasets: [
+        {
+          label: 'nbres de page lu par jour',
+          data: [3, 6, 9, 15,18, 2,4],
+          backgroundColor:'rgba(53, 162, 235, 0.5)',
+          borderColor: 'black',
+          borderWidth: 1,
+         }
+        ,
+        // {
+        //   label: '333',
+        //   data: [3, 3, 3],
+        //   backgroundColor:'red',
+        //   borderColor: 'black',
+        //   borderWidth: 1,
+        // }
+      ]
+    }
+console.log(data.datasets[0].data)
+    const options = {
 
-  const options = {
-
-  }
+    }
 
   return (
-    <div className="App">
-      <h1 style={ {padding: '20px'}}>comment creer un donut</h1>
-      <div>
-       <Doughnut
-       data = {data}
-       options = {options}
-       >
-        
-       </Doughnut>
-      </div>
+    <div className='app'>
+      <h1>Statistique de lecture</h1>
+     <div
+      style = {
+        {padding: '20px',
+        width: '100%'}
+      }
+     >
+      <Bar
+         data = {data}
+         options = {options}
+      ></Bar>
+     </div>
     </div>
   )
+  
 }
